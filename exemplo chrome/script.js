be1161a263546f0d146f6305bc19a5970000e9fc
julia-daniluski <julia.o.daniluski@gmail.com>
2025-02-03 10:35:50 -0300

    alert add

--- exemplo chrome/script.js
+++ exemplo chrome/script.js
@@ -1,1 +1,8 @@
-console.log("Olá mundo!");
+console.log("Olá mundo!"); //exibe uma mensagem no console. 
+alert("Meu site está linkado com o html") // aparece um alerta no site.
+
+// comentário de uma linha
+/* comentários
+de
+várias
+línguas.*/
