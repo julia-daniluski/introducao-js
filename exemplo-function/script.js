0180e3065d0d9b554020c96eb75bb43d4bd8b239
julia-daniluski <julia.o.daniluski@gmail.com>
2025-02-04 09:06:47 -0300

    function

---
+++ exemplo-function/script.js
@@ -0,0 +1,13 @@
+function teste (){
+    alert("I'll be right back!")
+}
+
+/* arrow function: 
+                  const teste= () => 
+
+Os dois tipos de funções são muito semelhantes, 
+então você pode perguntar por que as funções de seta foram introduzidas. 
+A grande diferença com funções regulares é quando elas são usadas como métodos de objeto. 
+Isso é algo que veremos em breve.*/
+
+teste ()
